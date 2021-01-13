7ea183d73f3ce6df1d66ae7339f9697304836368
Daryl <d.jacura@sky.com>
2021-01-13 19:52:02 +0000

    added cart context and delivery logic

--- AGame/settings.py
+++ AGame/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',  # Required by django-allauth
                 'django.contrib.messages.context_processors.messages',
+                'cart.contexts.cart_contents',
             ],
         },
     },
@@ -150,3 +151,5 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STANDARD_DELIVERY_PERCENTAGE = 10

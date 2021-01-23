6019382927c85c9b44604b9940d3d40bd636ab8a
Daryl <d.jacura@sky.com>
2021-01-23 08:47:52 +0000

    Added stripe_elements.js and CSS for stripe element

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -6,3 +6,25 @@
     https://stripe.com/docs/stripe-js
 */
 
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
+var elements = stripe.elements();
+var style = {
+    base: {
+      color: "#000",
+      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+      fontSmoothing: "antialiased",
+      fontSize: "16px",
+      "::placeholder": {
+        color: "#000"
+      }
+    },
+    invalid: {
+      color: "red",
+      iconColor: "red"
+    }
+  };
+
+var card = elements.create('card', {style: style});
+card.mount('#card-element');

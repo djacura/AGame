1594f7f280cfe1885f805ca12cccd64ffe516257
Daryl <d.jacura@sky.com>
2021-01-23 08:24:46 +0000

    Added stripe js to base.html and block js content

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HxqfMDnSE9zSCJz2oGm42Uft9J7C8K9GOWGlLwyvJrdEvBxwFLgYD6Zaori8dreKQ2lygv1DSXYAz2OGGmIRcmj00ZRR53quF',
+        'client_secret': 'test_key',
     }
 
     return render(request, template, context)

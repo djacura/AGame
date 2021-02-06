baa6fd754e4fe659a8083125dca84a609cded3f6
Daryl <d.jacura@sky.com>
2021-02-06 10:30:52 +0000

    Added game details template and view

---
+++ games/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.all_games, name='games'),
+    path('<game_id>', views.game_detail, name='game_detail'),
+]
--- games/views.py
+++ games/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Game
 
-# Create your views here.
+
+def all_games(request):
+    """ A view to show the Games """
+
+    games = Game.objects.all()
+
+    context = {
+        'games': games,
+    }
+
+    return render(request, 'games/games.html', context)
+
+
+def game_detail(request, game_id):
+    """ A view to see the Game detail page """
+
+    game = get_object_or_404(Game, pk=game_id)
+
+    context = {
+        'game': game,
+    }
+
+    return render(request, 'games/game_detail.html', context)

a0692e0f6e9d606b4dba55f3ce45b09e4e6c257d
Daryl <d.jacura@sky.com>
2021-02-14 11:58:47 +0000

    added membership amdin

--- AGame/urls.py
+++ AGame/urls.py
@@ -27,5 +27,4 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profiles/', include('profiles.urls')),
     path('games/', include('games.urls')),
-    path('memberships/', include('memberships.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- memberships/admin.py
+++ memberships/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Membership, UserMembership, Subscription
 
-# Register your models here.
+
+class UserMembershipAdmin(admin.ModelAdmin):
+    list_display = ('user', 'membership')
+
+
+admin.site.register(UserMembership, UserMembershipAdmin)
+admin.site.register(Membership)
+admin.site.register(Subscription)

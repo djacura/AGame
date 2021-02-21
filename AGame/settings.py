42116eff9751d9f3bb97bc691d922757feda4d6f
Daryl <d.jacura@sky.com>
2021-02-21 10:38:34 +0000

    Removed secret key and set debug

--- AGame/settings.py
+++ AGame/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@u=lqzcsu+5%erxv-*8lv4jckh95o-)379)_w+x!-bl625h_$_'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['agame-app-ms4.herokuapp.com', 'localhost']
 

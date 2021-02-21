adc3c4e4919f43c900bcf3a30d10e84dfe10b8b6
Daryl <d.jacura@sky.com>
2021-02-21 11:29:31 +0000

    Added cache control to settings

--- AGame/settings.py
+++ AGame/settings.py
@@ -181,8 +181,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STANDARD_DELIVERY_PERCENTAGE = 10
 SUB_DISCOUNT_PERCENTAGE = 10
 
-# S3 Bucket Config
+
 if 'USE_AWS' in os.environ:
+
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
+    # S3 Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'agame-app-ms4'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
